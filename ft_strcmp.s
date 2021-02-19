e9ea98e08f709c095ba21e3f076ad696278cc96e
Badrien19 <adrienbaar@hotmail.com>
2021-02-19 17:32:49 +0100

    should be good

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -5,24 +5,23 @@ ft_strcmp:                         				; ; s1 = rdi, s2 = rsi rax = le return
 				xor rcx, rcx					; met i a 0
 				xor rdx, rdx					; met tmp a 0
 loop:
-                mov dl, BYTE[rdi + rcx]         ;dl petite partie de rdx (tmp)
+				mov dl, BYTE[rdi + rcx]         ;dl petite partie de rdx (tmp)
                 cmp dl, BYTE[rsi + rcx]               
-                je increment
-                jl inferior                     ;si le cmp renvoi inferieur
-                jg superior                     ;si le cmp renvoi superieur
+                je increment					;on jump si c'est les même
+				jmp solve
 
 increment:
-                cmp dl, 0;                      ; check si fin de chaine de carac
-                je equal                        ; si oui = chaine identique
+                cmp dl, 0;                     ; check si fin des deux chaine de carac
+                jz solve                       ; si oui = chaine identique || jump 0
                 inc rcx
                 jmp loop
 
-superior:
-			mov		rax, 1
-			ret
-inferior:
-			mov		rax, -1
-			ret
-equal:
-			mov		rax, 0
+solve:		
+			sub dl, BYTE[rsi + rcx]
+			movzx rax, dl						; movzx = bzero + move
+			jnc end								; jnc flag d'erreur pas activer --> si overflow car negatif
+			neg al								; on doit repasser en négatif car pas la place dans dl
+			neg eax								; 
+
+end:
 			ret
